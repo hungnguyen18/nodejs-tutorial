06b90c3716409f37fd526ac0c6c16fc82ee18639
hung <hungtrewq3214@gmail.com>
2021-09-24 17:16:00 +0700

    create controller and routes

---
+++ src/app/controllers/NewsController.js
@@ -0,0 +1,13 @@
+class NewsController {
+
+    //[GET]/news
+    index(req, res) {
+        res.render('news');
+    };
+    //[GET]/news/:slug
+    show(req, res) {
+        res.render('covid');
+    };
+}
+
+module.exports = new NewsController;
--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
-
-
+const route = require('./routes');
+//read file public 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({
   extended: true
 }));
 app.use(express.json());
-//http 
+//http log
 app.use(morgan('combined'));
 
 
@@ -22,27 +22,13 @@ app.engine('hbs', handlebars({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, "resources/views"));
 
-app.get('/' , (req, res) => {
-  res.render('home');
-});
 
-app.get('/trangchu' , (req, res) => {
-  res.render('home');
-});
+//routes init
+route(app);
 
-app.get('/menu1', (req, res) => {
-  res.render('menu1');
-});
 
-app.get('/search', (req, res) => {
-  res.render('search');
-});
 
-app.post('/search', (req, res) => {
-  console.log(req.body);
-  res.send('da');
-});
-
+//console log
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
